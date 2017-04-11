f0a73bd03a1f65217cce78073478912e0da296f4
Ricardo García Fernández <ricardogarfe@gmail.com>
2017-04-11 17:55:48 +0200

    Initial commit

---
+++ src/main/java/com/example/geekshubs/CodeReviewsApplication.java
@@ -0,0 +1,12 @@
+package com.example.geekshubs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeReviewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeReviewsApplication.class, args);
+	}
+}
