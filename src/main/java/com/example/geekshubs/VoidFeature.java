52b770e948e12dc28cddb2edce944ba49bbb68fc
Ricardo García Fernández <ricardogarfe@gmail.com>
2018-05-24 21:03:06 +0200

    Create new issues

--- src/main/java/com/example/geekshubs/VoidFeature.java
+++ src/main/java/com/example/geekshubs/VoidFeature.java
@@ -1,8 +1,11 @@
 package com.example.geekshubs;
 
+import java.util.Map;
+
 public class VoidFeature {
 
   public void nothingToDo() {
+    System.out.println("Nothing to do here");
     System.out.println("Nothing to do here");
     System.out.println("Nothing to do here");
     System.out.println("Nothing to do here");
