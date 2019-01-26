82af56f9750ebe8f05e6258466a4f23bb4891c22
Ricardo García Fernández <ricardogarfe@gmail.com>
2019-01-26 11:31:59 +0100

    where is the wifi ?

--- src/main/java/com/example/geekshubs/DuplicatedVoidFeature.java
+++ src/main/java/com/example/geekshubs/DuplicatedVoidFeature.java
@@ -3,7 +3,14 @@ package com.example.geekshubs;
 public class DuplicatedVoidFeature {
 
   public void nothingToDo() {
-    System.out.println("Nothing to do here");
+    System.out.println("Where is the wifi??");
+    System.out.println("Where is the wifi??");
+    System.out.println("Where is the wifi??");
+    System.out.println("Where is the wifi??");
+    System.out.println("Where is the wifi??");
+    System.out.println("Where is the wifi??");
+    System.out.println("Where is the wifi??");
+    
     System.out.println("Nothing to do here");
     System.out.println("Nothing to do here");
     System.out.println("Nothing to do here");
